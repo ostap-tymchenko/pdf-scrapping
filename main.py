24604afa412836b7df8ec7270f046e0d307d46eb
ostap timur tymchenko <ostap.tymchenko@gmail.com>
2023-07-11 22:03:12 -0400

    end of day

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from PyPDF2 import PdfReader
-  
-# creating a pdf reader object
-reader = PdfReader('input.pdf')
-  
-# getting a specific page from the pdf file
+from pypdf import PdfReader
+
+reader = PdfReader("input.pdf")
+meta = reader.metadata
+
+# print("page length:", len(reader.pages))
+# print("author:", meta.author)
+# print("created in (software):", meta.creator)
+# print("producer:", meta.producer)
+# print("subject:", meta.subject)
+# print("title:", meta.title)
+
 a = []
 
-# a.append(reader.pages)
-# print(a)
 for page in reader.pages:
     a.append(page.extract_text())
 
-for line in a:
-    print(line)
+# for line in a:
+#     print("\n\n\n\n")
+#     print(line)
+
+def find_substring_in_array(substring, array):
+    return "\n".join(s for s in array if substring.lower() in s.lower())
+
+print(find_substring_in_array("Sales tax @", a))
+print(len(a))
